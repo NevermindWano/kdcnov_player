a83b76ba34ad77cc1be66ac1b2428fb985440538
Иван Попов <nevermind@kdcnov.local>
2017-12-08 20:22:10 +0300

    ИСПРАВЛЕНО issue#1 и issue#3

---
+++ KdcnovPlayer/Diagnostic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kdcnovAutoWinForms
+{
+    static class Diagnostic
+    {
+        private static System.Diagnostics.Stopwatch swatch;
+        public static void Start()
+        {
+            swatch = new System.Diagnostics.Stopwatch(); // создаем объект
+            swatch.Start(); // старт
+             
+            swatch.Stop(); // стоп
+            Console.WriteLine(swatch.Elapsed); // выводим результат в консоль
+        }
+
+        public static void Stop()
+        {
+            swatch.Stop(); // стоп
+            MessageBox.Show(swatch.Elapsed.ToString()); // выводим результат в консоль
+            swatch = null;
+        }
+
+    }
+}
