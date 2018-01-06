b35276d32d0413afd20335a9855dd819890e2bc0
Иван Попов <nevermind@kdcnov.local>
2018-01-06 23:56:35 +0300

    Перед выпиливанием SettingReader

---
+++ DataProvider/IKeyValueProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider
+{
+    public interface IKeyValueProvider
+    {
+        void Save<T>(string key, T value);
+        object Read(string key);
+        string[] GetAllKeys();
+    }
+}
